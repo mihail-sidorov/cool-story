105bdb25f447939a6b7e7214ce2e47d21815a2cc
Mihail <sidorov.mihail.maykop@mail.ru>
2020-12-10 16:18:13 +0300

    Сверстал 4-ый блок на странице отзывов для пк и мобильных.

--- resources/js/video-player-initial.js
+++ resources/js/video-player-initial.js
@@ -2,13 +2,14 @@ $(document).ready(() => {
     $('.video-player-initial').click((e) => {
         var video = e.currentTarget;
         var mediaVideo = $(video).find('video').get(0);
+        var btn = $(video).find('.video-btn').get(0);
         if (mediaVideo.paused) {
             mediaVideo.play();
-            $(video).addClass("plaing");
+            $(btn).addClass("plaing");
         }
         else {
             mediaVideo.pause();
-            $(video).removeClass("plaing");
+            $(btn).removeClass("plaing");
        }
      });
 });
